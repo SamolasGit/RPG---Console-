935fbe69ba4df6a977e59a09f80fe4afc2c86dc2
Samuel <samuelbaron07@gmail.com>
2026-05-06 20:06:23 -0300

    game loop created

--- game.cpp
+++ game.cpp
@@ -1,1 +1,25 @@
 #include<iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+
+    while (true)
+    {
+        string continuar;
+        cout << "Continuar? (sim/nao): ";
+        cin >> continuar;
+        if (continuar == "nao"){
+            break;
+        }
+    }
+    return 0;
+    
+
+
+
+
+}
+
